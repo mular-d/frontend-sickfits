59d5b720da5c2abf9efa1905016461e6df702176
Muluneh Derese <muluneh.derese@protonmail.com>
2022-06-14 16:27:28 +0300

    display list of products

--- components/Product.js
+++ components/Product.js
@@ -1,3 +1,21 @@
+import ItemStyles from './styles/ItemStyles';
+import formatMoney from '../lib/formatMoney';
+import Title from './styles/Title';
+import Link from 'next/link';
+import PriceTag from './styles/PriceTag';
+
 export default function Product({ product }) {
-  return <p>{product.name}</p>;
+  return (
+    <ItemStyles>
+      <img
+        src={product?.photo?.image?.publicUrlTransformed}
+        alt={product.name}
+      />
+      <Title>
+        <Link href={`/products/${product.id}`}>{product.name}</Link>
+      </Title>
+      <PriceTag>{formatMoney(product.price)}</PriceTag>
+      <p>{product.description}</p>
+    </ItemStyles>
+  );
 }
